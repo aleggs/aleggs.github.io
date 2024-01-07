0a8b13f7a83d6668897bc44426717e7cc6af32c2
Alex Xu <48495688+aleggs@users.noreply.github.com>
2024-01-07 18:16:20 -0500

    Revert "new free range lab grown website"
    This reverts commit 578355b2414ebb56c48fe0870e6c7f0a1c1165dc.

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import lit from "@astrojs/lit";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), lit()]
-});
